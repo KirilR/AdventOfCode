a120d690cf7af28a9b2bdf63e6cea8892b853947
Kiril Razpopov <kiril777@abv.bg>
2024-01-19 15:01:05 +0200

    Create TextFileReader.java

---
+++ AoC2022/AoC2022-4/TextFileReader.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextFileReader {
+
+    public String readFile(String filePath) {
+        StringBuilder fileContent = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                fileContent.append(line).append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            
+        }
+
+        return fileContent.toString();
+    }
+}
