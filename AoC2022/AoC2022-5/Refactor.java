59ffa5b3c46a0af93d9dbe2f218a3e4995843d4c
Kiril Razpopov <kiril777@abv.bg>
2024-01-19 15:23:49 +0200

    Update Refactor.java

--- AoC2022/AoC2022-5/Refactor.java
+++ AoC2022/AoC2022-5/Refactor.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
-//Both resolvements are here as two different methods
+//Both solutions are here as two different methods
 public class Refactor {
 
     ArrayList<Stack<Character>> ListOfstacks = new ArrayList<>();
