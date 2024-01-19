56b33d5085f4eace483f8ec1fd4a9a4c118a2616
Kiril Razpopov <kiril777@abv.bg>
2024-01-19 14:18:30 +0200

    Update AdvenOfCode2022App3.java

--- AoC2022/AoC2022-3/AdvenOfCode2022App3.java
+++ AoC2022/AoC2022-3/AdvenOfCode2022App3.java
@@ -1,1 +1,19 @@
+import java.io.IOException;
+
+public class AdvenOfCode2022App3 {
+
+    public static void main(String[] args) throws IOException {
+        TextFileReader reader = new TextFileReader();
+        String filePath = "C:\\Users\\Pavel\\Documents\\Kiro\\AdventOfCode3.2022.txt"; //file path
+        String fileTempPath = "C:\\Users\\Pavel\\Documents\\Kiro\\AdventOfCode3Temp.2022.txt";
+        String fileContent = reader.readFile(filePath);
+        String tempFileContent = reader.readFile(fileTempPath);
+        
+        RefactorTheFile Appa = new RefactorTheFile();
+        Appa.Refactor(fileContent);
+        RefactorTheFileSecondPart Appa2 = new RefactorTheFileSecondPart();
+        Appa2.Refactor(fileContent);
+
+    }
+}
 
